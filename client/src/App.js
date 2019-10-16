c0368baf44656b18f420ee4a07138d1cff4cabc4
Anas Bajjouk <bajjouk1@gmail.com>
2019-10-16 15:51:46 +0200

    Connecting react with redux auth state etc

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,33 @@ import React from 'react';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
 import ItemModal from './components/ItemModal';
+
 import { Container } from 'reactstrap';
 import { Provider } from 'react-redux';
+import { loadUser } from './actions/authAction';
+
 import store from './store';
 
 import './App.css';
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <AppNavbar />
-        <Container>
-          <ItemModal />
-          <ShoppingList />
-        </Container>
-      </div>
-    </Provider>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
